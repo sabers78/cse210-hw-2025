73c508d585e6bf5ac120cd9ef4014ca9803a6423
Nicolas Hopkins <seekerking123@gmail.com>
2025-01-11 19:43:39 -0700

    completed the grade code

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What percent did you get?");
+        string gradePercent = Console.ReadLine();
+
+        int numberPercent = int.Parse(gradePercent);
+
+        string finalGrade = ""; 
+
+        if (numberPercent >= 90)
+        {
+            finalGrade = "A";
+        }
+
+        else if (numberPercent >= 80)
+        {
+            finalGrade = "B";
+        }
+
+        else if (numberPercent >= 70)
+        {
+            finalGrade = "C";
+        }
+
+        else if (numberPercent >= 60)
+        {
+            finalGrade = "D";
+        }
+
+        else if (numberPercent < 60)
+        {
+            finalGrade = "F";
+        }
+
+
+        if (numberPercent >= 70)
+        {
+            Console.WriteLine($"Congratulations! You did well and passed with a {finalGrade}");
+        }
+
+        else 
+        {
+            Console.WriteLine("I'm sorry, you did not pass. Try again!");
+        }
+
+       
     }
 }
