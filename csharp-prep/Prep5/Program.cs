8770a4ad534ed021a7609abb94f350ac4527bd33
Nicolas Hopkins <seekerking123@gmail.com>
2025-01-18 17:33:26 -0700

    This was a lot easier after i figured out how to use functions. Thank you for staying after class for me.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,54 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+
+        DisplayWelcome();
+        string endName = PromptUsername();
+        int endNumber = PromptUserNumber();
+        int endSquare = SquareNumber(endNumber);
+        DisplayResult(endName, endSquare);
+
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+        //ask for and terutn the serers name as a string
+
+        static string PromptUsername()
+        {
+            Console.WriteLine("What is your name:");
+            string usersName = Console.ReadLine();
+            return usersName;
+
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.WriteLine("What is your favorite number?");
+            string userNumber = Console.ReadLine();
+            int realUserNumber = int.Parse(userNumber);
+
+            return realUserNumber;
+
+        }
+
+        //sqaure number function
+
+        static int SquareNumber(int realUserNumber)
+        {
+            int needToSqaure = realUserNumber * realUserNumber;
+            return needToSqaure;
+        }
+
+        static void DisplayResult(string usersName, int needToSqaure )
+        {
+            Console.WriteLine($"Your name is {usersName}, and your favorite number (squared) is {needToSqaure}");
+            
+        }
     }
 }
