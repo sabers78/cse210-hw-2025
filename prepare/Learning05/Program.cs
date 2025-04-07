c2c82bb19390ed2b8bb65a428b189b5b6bfc2891
Nicolas Hopkins <seekerking123@gmail.com>
2025-04-07 15:40:26 -0600

    I understand everything.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapeList = new List<Shape>();
+
+        Square shapeOne = new Square("Blue", 3);
+        Rectangle shapeTwo = new Rectangle("Yellow", 4,6);
+        Circle shapeThree = new Circle("Green", 23);
+
+        shapeList.Add(shapeOne);
+        shapeList.Add(shapeTwo);
+        shapeList.Add(shapeThree);
+
+        foreach (Shape _shape in shapeList)
+        {
+            string color = _shape.GetColor();
+            double area = _shape.GetArea();
+
+            Console.WriteLine($"There is a {color} shape with an area of {area}");
+        }
+       
     }
 }
